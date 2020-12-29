54cf38b91755097b796772d5677cfaac4898fc87
h0neybadger <h0neyb4dgerdc@gmail.com>
2020-12-29 18:08:24 +0100

    Fix deleted aws_instance on interface attachment
    https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/instance#network-interfaces

--- modules/aws_instance/main.tf
+++ modules/aws_instance/main.tf
@@ -31,45 +31,11 @@ data "aws_ami" "ami" {
   }
 }
 
-resource "aws_network_interface" "network_interface" {
-  for_each = {
-    for x in flatten(local.network_full):
-      format("i-%d-eni-%d", x.instance_index +1, x.device_index) => x
-  }
-
-  subnet_id = each.value.subnet_id
-  security_groups = each.value.security_groups
-  ipv6_address_count = each.value.ipv6_address_count
-  private_ips_count = each.value.private_ips_count
-
-  # due to count on instance object
-  # we cannot assign private_ip from this loop
-  # private_ips = each.value.private_ips
-
-  tags = merge(
-    {
-      "Name" = format("${local.instance_name_format}-eni-%d",
-        var.aws_instance_name, each.value.instance_index + 1, each.value.device_index)
-    },
-    var.tags,
-    var.aws_instance_tags,
-  )
-}
-
 resource "aws_instance" "instance" {
   count = var.aws_instance_count
 
   ami           = data.aws_ami.ami.id
   instance_type = var.aws_instance_instance_type
-
-  dynamic "network_interface" {
-    for_each = local.network_full[count.index]
-    iterator = each
-    content {
-      device_index         = each.value.device_index
-      network_interface_id = aws_network_interface.network_interface[format("i-%d-eni-%d", each.value.instance_index + 1, each.value.device_index)].id
-    }
-  }
 
   tags = merge(
     {
@@ -85,4 +51,35 @@ resource "aws_instance" "instance" {
     var.aws_instance_tags,
   )
 }
+
+resource "aws_network_interface" "network_interface" {
+  for_each = {
+    for x in flatten(local.network_full):
+      format("i-%d-eni-%d", x.instance_index +1, x.device_index) => x
+  }
+
+  subnet_id = each.value.subnet_id
+  security_groups = each.value.security_groups
+  ipv6_address_count = each.value.ipv6_address_count
+  private_ips_count = each.value.private_ips_count
+
+  # due to count on instance object
+  # we cannot assign private_ip from this loop
+  # private_ips = each.value.private_ips
+
+  attachment {
+    device_index = each.value.device_index
+    instance     = aws_instance.instance[each.value.instance_index].id
+  }
+
+  tags = merge(
+    {
+      "Name" = format("${local.instance_name_format}-eni-%d",
+        var.aws_instance_name, each.value.instance_index + 1, each.value.device_index)
+    },
+    var.tags,
+    var.aws_instance_tags,
+  )
+}
+
 
